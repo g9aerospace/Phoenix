1003fb33d7f0d9a2166d8d79366181e6fcd0b7f7
G9 Aerospace <80208426+g9militantsYT@users.noreply.github.com>
2023-12-20 00:48:05 +0530

    init

--- commands/ping.js
+++ commands/ping.js
@@ -1,16 +1,36 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const axios = require('axios');
+require('dotenv').config();
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Replies with Pong!'),
   async execute(interaction) {
-    const start = Date.now();
-    await interaction.reply({ content: 'Pinging...', ephemeral: true });
+    try {
+      // Start measuring the time
+      const start = Date.now();
 
-    const end = Date.now();
-    const latency = end - start;
+      // Send initial response
+      await interaction.reply({ content: 'Pinging...', ephemeral: true });
 
-    interaction.editReply(`Pong! Latency is ${latency}ms.`);
+      // End measuring the time
+      const end = Date.now();
+      const latency = end - start;
+
+      // Edit the initial response with detailed message
+      await interaction.editReply(`Pong! Latency is ${latency}ms.`);
+
+      // Log details to Discord webhook
+      const webhookURL = process.env.WEBHOOK_URL;
+      if (webhookURL) {
+        await axios.post(webhookURL, {
+          content: `Command /ping invoked by ${interaction.user.tag}. Latency: ${latency}ms.`,
+        });
+      }
+    } catch (error) {
+      console.error('Error:', error.message);
+      // You can handle the error as needed
+    }
   },
 };
--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
 const { config } = require('dotenv');
 const winston = require('winston');
+const axios = require('axios');
 const fs = require('fs');
 
 config(); // Load environment variables from .env file
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
-const commands = new Map();
 
 // Configure Winston logger
 const logger = winston.createLogger({
@@ -17,7 +15,7 @@ const logger = winston.createLogger({
     winston.format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`)
   ),
   transports: [
-    new winston.transports.Console(),
+    new winston.transports.Console({ format: winston.format.combine(winston.format.colorize(), winston.format.simple()) }),
     new winston.transports.File({ filename: 'logs/bot.log' }),
   ],
 });
@@ -31,6 +29,9 @@ async function registerGlobalSlashCommands() {
     const command = require(`./commands/${file}`);
     commandsData.push(command.data);
     commands.set(command.data.name, command);
+
+    // Log each loaded command in green
+    logger.info('\x1b[32m%s\x1b[0m', `Loaded command: ${command.data.name}`);
   }
 
   const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
@@ -39,31 +40,54 @@ async function registerGlobalSlashCommands() {
     logger.info('Started refreshing global (/) commands.');
 
     await rest.put(
-      Routes.applicationCommands(client.user.id),
+      Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commandsData },
     );
 
     logger.info('Successfully reloaded global (/) commands.');
   } catch (error) {
     logger.error(`Error refreshing global (/) commands: ${error.message}`);
+    logToWebhook(`Error refreshing global (/) commands: ${error.message}`);
   }
 }
 
+// Function to post logs to Discord webhook
+async function logToWebhook(message) {
+  const webhookURL = process.env.WEBHOOK_URL;
+
+  if (webhookURL) {
+    try {
+      // Post logs to the webhook
+      await axios.post(webhookURL, { content: `\`\`\`asciidoc\n${message}\n\`\`\`` });
+      logger.info('Logs successfully posted to the webhook.');
+    } catch (error) {
+      logger.error('Error posting logs to webhook:', error.message);
+    }
+  } else {
+    logger.warn('WEBHOOK_URL is not defined in the .env file. Logs will not be posted to a webhook.');
+  }
+}
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const commands = new Map();
+
 client.once('ready', async () => {
-  logger.info(`Logged in as ${client.user.tag}`);
-  
+  logToWebhook('Bot is starting...');
+  logger.info('\x1b[33m%s\x1b[0m', `Logged in as ${client.user.tag}`);
+
   // Register global slash commands on startup
   await registerGlobalSlashCommands();
 });
 
 client.on('messageCreate', (message) => {
-  logger.info(`Received message: ${message.content} from ${message.author.tag}`);
+  logger.info('\x1b[36m%s\x1b[0m', `Received message: ${message.content} from ${message.author.tag}`);
   // Your message handling logic here
 });
 
 client.on('interactionCreate', (interaction) => {
-  logger.info(`Received interaction: ${interaction.type} from ${interaction.user.tag}`);
-  
+  const startTime = Date.now();
+  logger.info('\x1b[35m%s\x1b[0m', `Received interaction: ${interaction.type} from ${interaction.user.tag}`);
+
   if (!interaction.isCommand()) return;
 
   const command = commands.get(interaction.commandName);
@@ -71,10 +95,32 @@ client.on('interactionCreate', (interaction) => {
 
   try {
     command.execute(interaction);
+
+    const endTime = Date.now();
+    const timeTaken = endTime - startTime;
+
+    logger.info(`Command ${interaction.commandName} executed in ${timeTaken}ms`);
   } catch (error) {
     logger.error(`Error executing command: ${error.message}`);
     interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
   }
 });
+
+// Log process errors
+process.on('unhandledRejection', (error) => {
+  logger.error(`Unhandled promise rejection: ${error}`);
+  logToWebhook(`Unhandled promise rejection: ${error}`);
+});
+
+process.on('uncaughtException', (error) => {
+  logger.error(`Uncaught exception: ${error}`);
+  logToWebhook(`Uncaught exception: ${error}`);
+});
+
+// Log metrics to webhook every minute
+setInterval(() => {
+  const logs = fs.readFileSync('logs/bot.log', 'utf8');
+  logToWebhook(logs);
+}, 60000);
 
 client.login(process.env.TOKEN);

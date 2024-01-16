f407f6d98dd05fb8fd14dcc240a3cb9c3746376b
g9militantsYT <g9militants@gmail.com>
2024-01-16 17:43:14 +0530

    needs interaction handler

--- commands/dropdown.js
+++ commands/dropdown.js
@@ -6,30 +6,32 @@ module.exports = {
         description: 'Send a dropdown menu to the user.',
     },
     async execute(interaction) {
-		const select = new StringSelectMenuBuilder()
-			.setCustomId('starter')
-			.setPlaceholder('Make a selection!')
-			.addOptions(
-				new StringSelectMenuOptionBuilder()
-					.setLabel('Bulbasaur')
-					.setDescription('The dual-type Grass/Poison Seed Pokémon.')
-					.setValue('bulbasaur'),
-				new StringSelectMenuOptionBuilder()
-					.setLabel('Charmander')
-					.setDescription('The Fire-type Lizard Pokémon.')
-					.setValue('charmander'),
-				new StringSelectMenuOptionBuilder()
-					.setLabel('Squirtle')
-					.setDescription('The Water-type Tiny Turtle Pokémon.')
-					.setValue('squirtle'),
-			);
+        const select = new StringSelectMenuBuilder()
+            .setCustomId('starter')
+            .setPlaceholder('Make a selection!')
+            .setMinValues(1)  // Minimum number of selected options (set to 1 to allow at least one selection)
+            .setMaxValues(3)  // Maximum number of selected options (set to the number of options to allow selecting all)
+            .addOptions(
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Bulbasaur')
+                    .setDescription('The dual-type Grass/Poison Seed Pokémon.')
+                    .setValue('bulbasaur'),
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Charmander')
+                    .setDescription('The Fire-type Lizard Pokémon.')
+                    .setValue('charmander'),
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Squirtle')
+                    .setDescription('The Water-type Tiny Turtle Pokémon.')
+                    .setValue('squirtle'),
+            );
 
-		const row = new ActionRowBuilder()
-			.addComponents(select);
+        const row = new ActionRowBuilder()
+            .addComponents(select);
 
-		await interaction.reply({
-			content: 'Choose your starter!',
-			components: [row],
-		});
-	},
+        await interaction.reply({
+            content: 'Choose your starter!',
+            components: [row],
+        });
+    },
 };

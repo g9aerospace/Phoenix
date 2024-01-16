b2877f886a58035fb8bd0cbd68d9228f900be967
g9militantsYT <g9militants@gmail.com>
2024-01-16 17:39:40 +0530

    some dropdown

---
+++ commands/dropdown.js
@@ -0,0 +1,35 @@
+const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: {
+        name: 'dropdown',
+        description: 'Send a dropdown menu to the user.',
+    },
+    async execute(interaction) {
+		const select = new StringSelectMenuBuilder()
+			.setCustomId('starter')
+			.setPlaceholder('Make a selection!')
+			.addOptions(
+				new StringSelectMenuOptionBuilder()
+					.setLabel('Bulbasaur')
+					.setDescription('The dual-type Grass/Poison Seed Pokémon.')
+					.setValue('bulbasaur'),
+				new StringSelectMenuOptionBuilder()
+					.setLabel('Charmander')
+					.setDescription('The Fire-type Lizard Pokémon.')
+					.setValue('charmander'),
+				new StringSelectMenuOptionBuilder()
+					.setLabel('Squirtle')
+					.setDescription('The Water-type Tiny Turtle Pokémon.')
+					.setValue('squirtle'),
+			);
+
+		const row = new ActionRowBuilder()
+			.addComponents(select);
+
+		await interaction.reply({
+			content: 'Choose your starter!',
+			components: [row],
+		});
+	},
+};
